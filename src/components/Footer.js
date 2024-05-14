096e6953f85d5309ac08fe2e55da01d11ea5a2ac
Temi Michael <olasehindemichael285@gmail.com>
2024-05-14 12:24:47 +0100

    New Commit for Hope In God Ent Web and Mobile App

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-    
+    return (
+        <div className="Footer">
+            <footer>
+                <p>Created by Lazy_Codes</p>
+            </footer>
+        </div>
+    )
 };
