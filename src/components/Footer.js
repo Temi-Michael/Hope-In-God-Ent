c23a063ffa650cb6b2d2f5a51e019a1ca375e5fa
Temi Michael <olasehindemichael285@gmail.com>
2024-05-18 16:26:46 +0100

    Time component added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Link } from "react-router-dom"
+import { useState, useEffect } from 'react'
+
+function Time() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000); // Update every second
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div>
+            <p>{time}</p>
+        </div>
+    );
+}
+
 
 export default function Footer() {
     return (
@@ -8,6 +30,7 @@ export default function Footer() {
             <footer>
                 <p>Created by Lazy_Codes</p><br></br><br></br>
                 <Link to='https://github.com/Temi-Michael/' target="_blank"><FontAwesomeIcon icon={faGithub} /></Link>
+                <Time />
             </footer>
         </div>
     )
