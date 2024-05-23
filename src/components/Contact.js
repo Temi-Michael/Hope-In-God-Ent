c1a1775029f60fa0275197e888ae8c39b8bddb73
Temi Michael <olasehindemichael285@gmail.com>
2024-05-23 15:49:14 +0100

    Added links to contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import { Link } from "react-router-dom"
 import { useState } from "react"
 
 export default function Contact() {
@@ -11,11 +15,12 @@ export default function Contact() {
     setName("")
     setEmail("")
     setComment("")
-    
+
   }
- 
+
+
   return (
-    <div className="contact">
+    <div className="ContactBody">
       <h1>Contact Us</h1>
       <form>
         <fieldset className="contact">
@@ -36,6 +41,25 @@ export default function Contact() {
           </div>
         </fieldset>
       </form>
+      <div className="addressinfo">
+        <p>
+          You can get in touch with us using our various platforms
+        </p>
+        <p className="numbers">
+          <FontAwesomeIcon icon={faPhone} /><span><Link to="tel:08063114697">+(234)-8063114697</Link></span>
+          <FontAwesomeIcon icon={faPhone} /><span><Link to="tel:08071668236">+(234)-8071668236</Link></span>
+          <FontAwesomeIcon icon={faPhone} /><span><Link to="tel:08066386024">+(234)- 8066386024</Link></span>
+        </p>
+        <p className="emailsend">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span>
+            <Link to="mailto:olasehindemichael285@gmail.com">Click to Send an Email</Link>
+          </span>
+        </p>
+        <p className="whatsapp">
+          <FontAwesomeIcon icon={faWhatsapp} /><span><Link to="https://wa.link/7j0gz2">+(234)-8071668236</Link></span>
+        </p>
+      </div>
     </div>
-  )  
+  )
 };
